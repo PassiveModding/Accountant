e4b624fe7a427e27444a9685faf76669eb834cba
Passive <passivemodding@gmail.com>
2022-07-16 12:53:37 +1000

    Disable timer window options
    Added options to disable the timer window during combat, instance and cutscene

--- Accountant/AccountantConfiguration.cs
+++ Accountant/AccountantConfiguration.cs
@@ -31,6 +31,9 @@ public enum ConfigFlags : uint
     NoHeaderStyling    = 0x020000,
     CustomDelivery     = 0x040000,
     Tribes             = 0x080000,
+    NoTimerInCombat    = 0x100000,
+    NoTimersInIntance  = 0x200000,
+    NoTimersInCutscene = 0x400000
 }
 
 public static class ConfigFlagExtensions
@@ -76,7 +79,10 @@ public class AccountantConfiguration : IPluginConfiguration
       | ConfigFlags.JumboCactpot
       | ConfigFlags.LeveAllowances
       | ConfigFlags.CustomDelivery
-      | ConfigFlags.Tribes;
+      | ConfigFlags.Tribes
+      | ConfigFlags.NoTimerInCombat
+      | ConfigFlags.NoTimersInIntance
+      | ConfigFlags.NoTimersInCutscene;
 
     public int Version { get; set; } = 4;
 
@@ -93,6 +99,24 @@ public class AccountantConfiguration : IPluginConfiguration
     {
         get => Flags.Check(ConfigFlags.WindowVisible);
         set => Flags.Set(ConfigFlags.WindowVisible, value);
+    }
+
+    public bool NoTimerWindowInCombat
+    {
+        get => Flags.Check(ConfigFlags.NoTimerInCombat);
+        set => Flags.Set(ConfigFlags.NoTimerInCombat, value);
+    }
+
+    public bool NoTimerWindowInInstance
+    {
+        get => Flags.Check(ConfigFlags.NoTimersInIntance);
+        set => Flags.Set(ConfigFlags.NoTimersInIntance, value);
+    }
+
+    public bool NoTimerWindowDuringCutscene
+    {
+        get => Flags.Check(ConfigFlags.NoTimersInCutscene);
+        set => Flags.Set(ConfigFlags.NoTimersInCutscene, value);
     }
 
     public bool EnableRetainers
